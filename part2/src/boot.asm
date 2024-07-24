8334f30b419d4e8a9ebe66e1d9819c7cde7548ac
PTFOPlayer <patryk041103@gmail.com>
2024-07-25 00:16:38 +0200

    checking multiboot

--- part2/src/boot.asm
+++ part2/src/boot.asm
@@ -1,16 +1,20 @@
 ; definicja miejsca zaczęcia programu
-global start
+global  start
 
 section .text
 
 ; informacja dla kompilatora o tym że wszystkie poniższe instrukcje
 ; muszą być zakodowane w systemie 32 bitowym, stało rozmiarowym
-bits 32
+bits    32
 start:
     ; inicjacja stacku
     ; w procesorach potomnych dla i386 stack zaczyna się od jego góry i kończy na spodzie
     mov esp, stack_top
-
+    
+    ; w celu testu procedury sprawdzenia multiboot należy odkomentować poniższą linijkę
+    ; mov eax, 0
+    ; wywołanie procedury sprawdzenia multiboot
+    call multiboot_check
 
     ; wypisanie "OK" na ekranie
     ; adres 0x000b8000 jest na tym etapie adresem danych wypisywanych w trybie tekstowym
@@ -32,8 +36,27 @@ error:
     mov dword [0xb8000], 0x4f524f45
     mov dword [0xb8004], 0x4f3a4f52
     mov dword [0xb8008], 0x4f204f20
-    mov byte  [0xb800a], al
+    mov byte [0xb800a],  al
     hlt
+
+; procedura odpowiedzialna za sprawdzenie czy kernel był odpalony z systemu multiboot
+; potem będziemy wykorzystywać niektóre własciwości tego systemu
+multiboot_check:
+    ; z dokumentacji multiboot wiemy że bootloader ładuje na start wartość do rejestru EAX
+    ; EAX = 0x36d76289
+    ; jeśli w EAX jest ta wartość to korzystamy z multiboot
+    ; w przeciwnym razie w EAX będzie 0 ponieważ domyślny start jest na pustych rejestrach
+    cmp eax, 0x36d76289
+    ; jeśli nie równe, skaczemu do wywołania błędu
+    jne .multiboot_error
+    ret
+    
+    ; jeśli procedura nie zadziała poprawnie musimy zgłosić błąd
+.multiboot_error:
+    mov  al, "0"
+    call error
+
+
 
 ; sekcja danych statycznych
 section .bss
