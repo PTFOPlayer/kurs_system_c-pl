e38791f229034d21f8892a7a1fd98a3a0dfef282
PTFOPlayer <patryk041103@gmail.com>
2024-11-24 19:42:48 +0100

    finishing code for new part 1
    needs:
    * comments
    * simplification
    * documentation

--- part1/src/boot.asm
+++ part1/src/boot.asm
@@ -1,5 +1,6 @@
-bits    32
-
+%include "src/boot/boot_tests.asm"
+%include "src/boot/gdt.asm"
+%include "src/boot/paging.asm"
 section .multiboot_header
 header_start:
     dd 0xe85250d6                
@@ -16,11 +17,36 @@ header_end:
 
 global start
 section .text
+bits    32
 start:
     mov esp, stack_top
+    
+    call multiboot_check
+    call cpuid_check
+    call long_mode_check
+    
+    
+    call set_up_paging
+    call enable_paging
+
+    lgdt [gdt.Pointer]
+    jmp gdt.Code:long_mode
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
 
+bits 64
+
+long_mode:
+    cli
+    mov ax, gdt.Data
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov rax, 0x2f592f412f4b2f4f
+    mov qword [0xb8000], rax
+    hlt
 
 
 section .bss
@@ -28,3 +54,10 @@ align 16
 stack_bottom:
     resb 16384 ; 16KiB
 stack_top:
+align 4096
+p4_table:
+    resb 4096
+p3_table:
+    resb 4096
+p2_table:
+    resb 4096
