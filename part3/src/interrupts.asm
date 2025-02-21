913064d4d908013a25f7efb11af326cacf159ba1
PTFOPlayer <patryk041103@gmail.com>
2025-02-21 08:18:44 +0100

    Working keyboard

--- part3/src/interrupts.asm
+++ part3/src/interrupts.asm
@@ -1,12 +1,63 @@
+%macro pushaq 0
+    push rax
+    push rcx
+    push rdx
+    push rbx
+    push rbp
+    push rsi
+    push rdi
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+%endmacro
+
+%macro popaq 0
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdi
+    pop rsi
+    pop rbp
+    pop rbx
+    pop rdx
+    pop rcx
+    pop rax
+%endmacro
+
+
 %macro isr_err_stub 1
 isr_stub_%+%1:
+    pushaq
+    cli
+    pushf
+    push %1
+    mov rdi, rsp
     call exception_handler
+    pop rdi
+    popaq
     iretq
 %endmacro
 
 %macro isr_no_err_stub 1
 isr_stub_%+%1:
+    pushaq
+    cli
+    pushf
+    push %1
+    mov rdi, rsp 
     call exception_handler
+    pop rdi
+    popaq
     iretq
 %endmacro
 
@@ -49,5 +100,47 @@ isr_stub_table:
 %assign i 0 
 %rep    32 
     dq isr_stub_%+i
+%assign i i+1 
+%endrep
+
+%macro irq 1
+irq_%1:
+    cli
+    push rbp
+    mov rbp, rsp
+    pushfq
+    pushaq
+    mov rdi, %1
+    call irq_handler
+    popaq
+    popfq
+    pop rbp
+    sti
+    ret
+%endmacro
+
+extern irq_handler
+irq 0
+irq 1
+irq 2
+irq 3
+irq 4
+irq 5
+irq 6
+irq 7
+irq 8
+irq 9
+irq 10
+irq 11
+irq 12
+irq 13
+irq 14
+irq 15
+
+global irq_stub_table
+irq_stub_table:
+%assign i 0 
+%rep    16
+    dq irq_%+i
 %assign i i+1 
 %endrep
