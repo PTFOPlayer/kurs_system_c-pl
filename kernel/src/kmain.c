bf25f07e5d8c2d318b8a23d7a37a12a2b9d73976
PTFOPlayer <patryk041103@gmail.com>
2025-03-04 09:06:05 +0100

    Implementing frame allocator

--- kernel/src/kmain.c
+++ kernel/src/kmain.c
@@ -5,7 +5,7 @@
 #include "serial/serial.h"
 #include "serial/serial_dbg.h"
 #include "timers/pit.h"
-
+#include "memory/frame_allocator.h"
 // plik multiboot z strony OSDEV
 #include "./multiboot/multiboot.h"
 
@@ -22,18 +22,37 @@ void pit_handle() {
 
 void kmain(void *multiboot_info) {
     init_writer(LightGray, Black);
-    
     idt_init();
+    init_serial(COM1, 0);    
     init_keyboard();
     
-    MultibootData data = parse_multiboot(multiboot_info, 1);
-    print_mmap(data.mmap_tag);
-    init_serial(COM1, 0);
-    serial_dbg(COM1);
-
-    set_pit_handler(pit_handle);
     init_pit(100);
+    set_pit_handler(pit_handle);
+    
+    serial_dbg(COM1);
+    
+    MultibootData data = parse_multiboot(multiboot_info, 1);
 
+    
+    AreaFrameAllocator allocator = new_allocator(&data);
+
+    for (i32 i = 0; i < 256; i++)
+    {
+        Frame f;
+        AllocatorResult res = allocate_frame(&allocator, &f);
+        if (res != Success)
+        {
+            printf("Alocation error");
+            break;
+        }
+        
+        printf("%d\t", f.number);
+
+        if(i%8 == 0) {
+            printf("\n");
+        }
+    }
+    
         
     while (1) {
     }
--- kernel/src/multiboot/multiboot_parse.h
+++ kernel/src/multiboot/multiboot_parse.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "../stdlib/stdint.h"
 #include "../stdlib/string.h"
 #include "../vga/vga.h"
@@ -28,19 +29,55 @@ char *segment_tags[] = {
     "image load base physical address",
 };
 
-void print_mmap(struct multiboot_tag_mmap *tag) {
-    puts("\n\nMMAP\n");
+struct multiboot_elf_sections_entry {
+    u32 name;
+    u32 type;
+    u64 flags;
+    u64 addr;
+    u64 offset;
+    u64 size;
+    u32 link;
+    u32 info;
+    u64 addralign;
+    u64 entsize;
+} __attribute__((packed));
 
-    multiboot_memory_map_t *mmap;
-    char buffer[32] = {0};
-    for (mmap = tag->entries; (u8 *)mmap < (u8 *)tag + tag->size;
-         mmap = (multiboot_memory_map_t *)((u64)mmap + tag->entry_size)) {
-        printf("\t\tStart: 0x%x Len: %x\n", mmap->addr, mmap->len);
+void print_kernel_sections(struct multiboot_tag_elf_sections *tag) {
+    printf("\n\nELF Kernel Sections\n");
+
+    struct multiboot_elf_sections_entry *section =
+        (struct multiboot_elf_sections_entry *)(tag->sections);
+
+    u64 kernel_start_addr = __UINT64_MAX__;
+    u64 kernel_end_addr = 0;
+    for (u32 i = 0; i < tag->num; i++) {
+        printf("\t\tAddr: 0x%x, Size: %x, Type: %d, Flags: 0x%x\n",
+               section[i].addr, section[i].size, section[i].type,
+               section[i].flags);
+        if (section[i].addr != 0 && section[i].addr < kernel_start_addr)
+            kernel_start_addr = section[i].addr;
+        if (section[i].addr + section[i].size > kernel_end_addr)
+            kernel_end_addr = section[i].addr + section[i].size;
     }
-};
+
+    printf("Kernel start: 0x%x, Kernel end: 0x%x", kernel_start_addr,
+           kernel_end_addr);
+}
+
+#define MAX_MEMORY_AREAS 128
 
 typedef struct MultibootData {
-    struct multiboot_tag_mmap *mmap_tag;
+    u64 multiboot_information_start;
+    u64 multiboot_information_end;
+    struct mmap {
+        multiboot_memory_map_t mmap[MAX_MEMORY_AREAS];
+        u32 mmap_size;
+    } mmap;
+    struct kernel_addr {
+        u64 start;
+        u64 end;
+    } kernel_addr;
+
 } MultibootData;
 
 MultibootData parse_multiboot(void *mbd, u8 debug) {
@@ -55,6 +92,8 @@ MultibootData parse_multiboot(void *mbd, u8 debug) {
     }
 
     MultibootData data;
+    data.multiboot_information_start = (u64)mbd;
+    data.multiboot_information_end = (u64)mbd + info_size;
 
     struct multiboot_tag *tag;
     for (u32 i = 8; i < info_size;) {
@@ -66,9 +105,51 @@ MultibootData parse_multiboot(void *mbd, u8 debug) {
 
         i += (size + 7) & ~7;
 
-        if (type == MULTIBOOT_TAG_TYPE_MMAP) {
-            data.mmap_tag = (struct multiboot_tag_mmap *)tag;
+        switch (type) {
+            case MULTIBOOT_TAG_TYPE_MMAP:
+                struct multiboot_tag_mmap *mmap_tag =
+                    (struct multiboot_tag_mmap *)tag;
+                multiboot_memory_map_t *mmap;
+                for (mmap = mmap_tag->entries;
+                     (u8 *)mmap < (u8 *)mmap_tag + mmap_tag->size;
+                     mmap = (multiboot_memory_map_t *)((u64)mmap +
+                                                       mmap_tag->entry_size)) {
+                    data.mmap.mmap[data.mmap.mmap_size] = *mmap;
+                    data.mmap.mmap_size++;
+                }
+
+                break;
+            case MULTIBOOT_TAG_TYPE_ELF_SECTIONS:
+                struct multiboot_tag_elf_sections *sections_tag =
+                    (struct multiboot_tag_elf_sections *)tag;
+
+                struct multiboot_elf_sections_entry *section =
+                    (struct multiboot_elf_sections_entry *)(sections_tag
+                                                                ->sections);
+                u64 kernel_start_addr = __UINT64_MAX__;
+                u64 kernel_end_addr = 0;
+                for (u32 i = 0; i < sections_tag->num; i++) {
+                    if (section[i].addr != 0 &&
+                        section[i].addr < kernel_start_addr)
+                        kernel_start_addr = section[i].addr;
+                    if (section[i].addr + section[i].size > kernel_end_addr)
+                        kernel_end_addr = section[i].addr + section[i].size;
+                }
+
+                data.kernel_addr.start = kernel_start_addr;
+                data.kernel_addr.end = kernel_end_addr;
+            default:
+                break;
         }
     }
     return data;
 }
+
+void print_mmap(struct MultibootData *data) {
+    printf("\n\nMMAP\n");
+
+    for (u32 i = 0; i < data->mmap.mmap_size; i++) {
+        printf("\t\tStart: 0x%x Len: %x Type: %d\n", data->mmap.mmap[i].addr,
+               data->mmap.mmap[i].len, data->mmap.mmap[i].type);
+    }
+};
